69ca72c13db4fd47bfa016e78985f1116a031f44
Michał Łabucki <labucki.michal@gmail.com>
2026-01-08 15:54:10 +0100

    refactor: removed total replaced by return statement for every condition

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,17 +9,14 @@ function calculateRentalCost(days) {
   const discountSevenDays = 50;
   const threeDays = 3;
   const sevenDays = 7;
-  let total = 0;
 
   if (days >= sevenDays) {
-    total = days * pricePerDay - discountSevenDays;
+    return days * pricePerDay - discountSevenDays;
   } else if (days >= threeDays && days < sevenDays) {
-    total = days * pricePerDay - discountThreeDays;
+    return days * pricePerDay - discountThreeDays;
   } else {
-    total = days * pricePerDay;
+    return days * pricePerDay;
   }
-
-  return total;
 }
 
 module.exports = calculateRentalCost;
